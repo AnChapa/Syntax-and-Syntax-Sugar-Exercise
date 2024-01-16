a340aa7ac48b6694233352a365df8b5fa740c02f
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-16 11:52:20 -0600

    Initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise1
+            Console.WriteLine("What number is less than 9:");
+            var response = int.Parse(Console.ReadLine());
+            string answer = (response < 9) ? " is less than 9." : " is greater than or equal to 9.";
+            Console.WriteLine($"{response}{answer}");
         }
     }
 }
